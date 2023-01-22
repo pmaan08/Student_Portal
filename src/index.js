acaba6ad7f86dbff3a9132a0e473e85a0781ce88
pmann08 <partulmann@gmail.com>
2023-01-22 21:41:37 +0530

    main route

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './index.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+function Main() {
+  return <h1>Main Route</h1>;
+}
+
+function App() {
+  return (
+    <Router>
+      <Route path="/" exact component={Main} />
+    </Router>
+  );
+}
+
+export default App;
